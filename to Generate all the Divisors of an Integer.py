12b210d39a94c913997cdb6e1fb56c92b76f289b
sejall <33982474+sejall@users.noreply.github.com>
2018-09-19 07:06:45 +0530

    Add files via upload

---
+++ to Generate all the Divisors of an Integer.py
@@ -0,0 +1,9 @@
+
+
+#to Generate all the Divisors of an Integer
+
+n=int(input("Enter an integer:"))
+print("The divisors of the number are:")
+for i in range(1,n+1):
+    if(n%i==0):
+        print(i)
